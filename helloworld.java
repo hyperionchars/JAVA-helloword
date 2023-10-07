a21a048a41e24eeec80954ef521468d2de1111a6
brais.americano <brais.americano@gmail.com>
2023-10-07 22:20:18 +0200

    Code as an example for java beginners

---
+++ helloworld.java
@@ -0,0 +1,32 @@
+/**
+ * This is a recommended comment format for all your JAVA applications.
+ * Here you can indicate the description of the class or method. In this case we only have one class.
+ *
+ * This JAVA application allows you to print the text: <Hello World> through the console right after its execution.
+ *
+ * Remember, as we are getting started, in order to run this code you need to open a command console instance.
+ * Place the prompt of this instance in the directory of this file.java and execute the [javac helloworld.java] command to compile it.
+ * Then execute the [java helloworld] command. This last command will be responsible for running our application (helloworld.class).
+ *
+ * Note: in order to execute the command [javac ...] it is necessary to have previously installed
+ * the java developer kit that best suits us from the official java website.
+ *
+ * You can use tags such as @param, @return, and @throws to document the parameters, return value, and exceptions thrown by a method.
+ *
+ * @param parameter Parameter description
+ * @return Return value description
+ * @throws ExceptionType Description of the exception thrown
+ *
+ * In this case we haven't additional methods in the main program, no return data or useful parameters to transfer to our program.
+ *
+ */
+class helloworld {
+
+    // The following statement is used in a general way to host the execution of our sequential execution program.
+    public static void main (String[] args){
+
+        // The following line of code is responsible for printing the text between " " through the console.
+        System.out.print("Hello World");
+
+    }
+}
